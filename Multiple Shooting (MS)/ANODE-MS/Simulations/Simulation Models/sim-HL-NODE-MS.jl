d090f3d34ee5c33bf9680b56357d927477e30dcf
junetroan <junetroan@gmail.com>
2024-02-06 14:09:57 +0100

    Changes to sim-HL-NODE-MS and sim-LV-NODE-MS files

--- Multiple Shooting (MS)/ANODE-MS/Simulations/Simulation Models/sim-HL-NODE-MS.jl
+++ Multiple Shooting (MS)/ANODE-MS/Simulations/Simulation Models/sim-HL-NODE-MS.jl
@@ -10,15 +10,18 @@ using StableRNGs
 using Random
 using CSV, Tables, DataFrames
 using StatsBase
+gr()
 
 function trueODEfunc(du, u, p, t)
     α, β, γ, δ = p
     du[1] = α * u[1] - β * u[2] * u[1]
     du[2] = γ * u[1] * u[2] - δ * u[2]
 end
-data_path = "Data/lynx_hare_data.csv"
+
+data_path = "Multiple Shooting (MS)/ANODE-MS/Data/lynx_hare_data.csv"
 data = CSV.read(data_path, DataFrame)
 rng = StableRNG(1111)
+
 #Train/test Splits
 split_ration = 0.75
 train = data[1:Int(round(split_ration*size(data, 1))), :]
@@ -35,14 +38,11 @@ X_new = reshape(X_train, 1, :)
 unknown_X = zeros(Float32, 1, 68)
 X = vcat(X_new, unknown_X)
 
-
-
 t = Float32.(collect(1:size(data, 1)))
 t_train = Float32.(collect(1:Int(round(split_ration*size(data, 1)))))
 t_test = Float32.(collect(Int(round(split_ration*size(data, 1))):size(data, 1)))
 
 # Define the experimental parameter
-
 group_size = 2
 state = 2
 iters = 2
@@ -56,48 +56,63 @@ fulltraj_losses = Float32[]
 
 @time begin
     for i in 1:iters
-        
+
         println("Simulation $i")
 
         # Random numbers
-        rng_i = StableRNG(i)
+        rng_1 = StableRNG(i)
+        rng_2 = StableRNG(i + 1)
 
         # Define the Neural Network
         nn = Lux.Chain(Lux.Dense(state, 30, tanh), Lux.Dense(30, state))
-        p_init, st = Lux.setup(rng_i, nn)
+        p_init, st = Lux.setup(rng_1, nn)
 
-        neuralode = NeuralODE(nn, tspan, AutoTsit5(Rosenbrock23(autodiff = false)), saveat = tsteps, sensealg = InterpolatingAdjoint(autojacvec = ReverseDiffVJP(true)), abstol = 1f-6, reltol = 1f-6)
-        prob_node = ODEProblem((u,p,t) -> nn(u,p,st)[1], [[u0[1];zeros(state-1)]], tspan, ComponentArray(p_init))
+        params = ComponentVector{Float32}(ANN = p_init, u0_states = 0.5*randn(rng_2, Float32, state - 1))
+
+        neuralode = NeuralODE(nn, tspan, AutoTsit5(Rosenbrock23(autodiff = false)), saveat = tsteps,
+         abstol = 1f-6, reltol = 1f-6, sensealg = InterpolatingAdjoint(autojacvec = ReverseDiffVJP(true)))
+        
+        function rhs(du, u, p, t)
+            du[1:end] = nn(u, p.ANN, st)[1]
+        end
+        
+        prob_node = ODEProblem(rhs, [u0[1]; params.u0_states], tspan, params, saveat = tsteps)
+
+        solve(prob_node, AutoTsit5(Rosenbrock23(autodiff = false)), saveat = tsteps, abstol = 1f-6, reltol = 1f-6)
 
         function loss_function(data, pred)
-            return mean(abs2, data - pred)
+            return mean(abs2, data[1, :] - pred[1, :])
         end
 
         function continuity_loss_function(u_end, u_0)
             return mean(abs2, u_end - u_0)
         end
-        
+
         function loss_multiple_shooting(p)
-            return multiple_shoot(p, X, tsteps, prob_node, loss_function,  continuity_loss_function, AutoTsit5(Rosenbrock23(autodiff=false)),
-                                  group_size; continuity_term, abstol = 1f-6, reltol = 1f-6)
+            new_prob = remake(prob_node, u0 = [u0[1]; p.u0_states], p = p)
+            return multiple_shoot(p, X, tsteps, new_prob, loss_function, continuity_loss_function, AutoTsit5(Rosenbrock23(autodiff = false)),
+                                group_size; continuity_term, abstol = 1f-6, reltol = 1f-6, sensealg = InterpolatingAdjoint(autojacvec = ReverseDiffVJP(true)))
         end
         
-        function predict_final(θ)
-            return Array(neuralode([u0[1]; zeros(state -1)], θ, st)[1])
+        function predict_final(p)
+            prob_new = remake(prob_node, u0 = [u0[1]; p.u0_states], p = p)
+            return Array(solve(prob_new, AutoTsit5(Rosenbrock23(autodiff = false)), saveat = tsteps, abstol = 1f-6, reltol = 1f-6))[1, :]
         end
-        
+
         function final_loss(θ)
             X̂ = predict_final(θ)
-            prediction_error = mean(abs2, X[1,:] .- X̂[1, :])
+            prediction_error = mean(abs2, X[1, :] .- X̂)
             prediction_error
         end
-        
+
+
         function predict_single_shooting(p)
-            return Array(neuralode([[u0[1];zeros(state-1)]], p, st)[1])
+            prob_new = remake(prob_node, u0 = [u0[1]; p.u0_states], p = p)
+            return Array(solve(prob_new, AutoTsit5(Rosenbrock23(autodiff = false)), saveat = tsteps, abstol = 1f-6, reltol = 1f-6))[1, :]
         end
-        
 
         losses = Float32[]
+
         callback = function (p, l, preds; doplot = false)
             push!(losses, final_loss(p))
             if length(losses) % 50 == 0
@@ -109,35 +124,12 @@ fulltraj_losses = Float32[]
 
         adtype = Optimization.AutoZygote()
         optf = Optimization.OptimizationFunction((x,p) -> loss_multiple_shooting(x), adtype)
-        optprob = Optimization.OptimizationProblem(optf, ComponentArray(p_init))
-        @time res_ms = Optimization.solve(optprob, ADAM(), maxiters=5000; callback = callback)
+        optprob = Optimization.OptimizationProblem(optf, params)
+        @time res_ms = Optimization.solve(optprob, ADAM(), maxiters = 5000; callback = callback)
 
         losses_df = DataFrame(loss = losses)
-        CSV.write("Simulations/Results/sim-HL-NODE-MS/Loss Data/Losses $i.csv", losses_df, writeheader = false)
+        CSV.write("Multiple Shooting (MS)/ANODE-MS/Simulations/Results/sim-HL-NODE-MS/Loss Data/Losses $i.csv", losses_df, writeheader = false)
 
         # Evaluate Single Shooting
         function loss_single_shooting(p)
-            pred = predict_single_shooting(p)
-            l = loss_function(Xₙ, pred)
-            return l, pred
-        end
-
-        full_traj = predict_final(res_ms.u)
-        full_traj_loss = final_loss(res_ms.u)
-        push!(fulltraj_losses, full_traj_loss)
-
-        function plot_results(real, pred)
-            plot(t_train, pred[1,:], label = "Training Prediction", title="Iteration $i of Randomised NODE-MS Model", xlabel="Time", ylabel="Population")
-            scatter!(t_train, real[1,:], label = "Training Data")
-            plot!(legend=:topright)
-            savefig("Simulations/Results/sim-HL-NODE-MS/Plots/Simulation $i.png")
-        end
-
-        plot_results(X, full_traj)
-
-        if i==iters
-            println("Simulation finished")
-            break 
-        end
-    end
-end
+    
