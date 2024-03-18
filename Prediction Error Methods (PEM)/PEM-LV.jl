4384b758d319d930beabda7bcd5970d33725e3a5
junetroan <junetroan@gmail.com>
2024-03-18 13:17:29 +0100

    Updated PEM-LV with plot for training and testing

--- Prediction Error Methods (PEM)/PEM-LV.jl
+++ Prediction Error Methods (PEM)/PEM-LV.jl
@@ -11,7 +11,6 @@ using DiffEqFlux
 using Plots
 plotly()
 
-
 # Producing data
 function lotka!(du, u, p, t)
     α, β, γ, δ = p
@@ -32,7 +31,6 @@ t = solution.t
 tspan = (t[1], t[end])
 tsteps = range(tspan[1], tspan[2], length = length(X[1,:]))
 
-
 # Adding noise
 x̄ = mean(X, dims = 2)
 noise_magnitude = 10f-3
@@ -40,15 +38,14 @@ noise_magnitude2 = 62.3f-2
 Xₙ = X .+ (noise_magnitude * x̄) .* randn(rng, eltype(X), size(X))
 y = Xₙ[1,:]
 
-
 # Definition of neural network
 state = 2
 U = Lux.Chain(Lux.Dense(state, 30, tanh),Lux.Dense(30, state))
 p, st = Lux.setup(rng1, U)
-K = Float32[0.214566, 0.928355]
-
-
+K = randn(rng2, Float32, state)
+y_zoh = ConstantInterpolation(y, tsteps)
 #Definition of the model 
+
 function predictor!(du,u,p,t)
     û = U(u, p.vector_field_model, st)[1]
     yt = y_zoh(t)
@@ -57,7 +54,6 @@ function predictor!(du,u,p,t)
 end
 
 params = ComponentVector{Float32}(vector_field_model = p, K = K)
-#ps = getdata(params)
 prob_nn = ODEProblem(predictor!, u0 , tspan, params, saveat=tsteps)
 soln_nn = Array(solve(prob_nn, Tsit5(), abstol = 1e-8, reltol = 1e-8, saveat = 0.25f0))
 
@@ -90,11 +86,52 @@ end
 adtype = Optimization.AutoZygote()
 optf = Optimization.OptimizationFunction((x,p) -> predloss(x), adtype)
 optprob = Optimization.OptimizationProblem(optf, params)
-@time res_ms = Optimization.solve(optprob, ADAM(), maxiters = 5000, verbose = false)
+@time res_ms = Optimization.solve(optprob, ADAM(), maxiters = 5000, verbose = false, callback = callback)
 
 # Predictions
-y_pred = prediction(res_pred.u)
+y_pred = prediction(res_ms.u)
 
 plot(y_pred[1,:])
+scatter!(y)
+
+full_traj_loss = predloss(res_ms.u)
+println("Full Trajectory Loss: ",full_traj_loss)    
+
+optf_final = Optimization.OptimizationFunction((x,p) -> predloss(x), adtype)
+optprob_final = Optimization.OptimizationProblem(optf_final, res_ms.u)
+res_final = Optimization.solve(optprob_final, BFGS(initial_stepnorm=0.01), maxiters = 1000, callback=callback, allow_f_increases = true)
+
+print(losses[end])
+
+full_traj2 = prediction(res_final.u)
+actual_loss = Xₙ[1,:] - full_traj2[1,:]
+total_loss = abs(sum(actual_loss))
+
+plot(full_traj2[1, :])
+scatter!(y)
 
 
+#############################################################################################
+# Testing 
+p_ = Float32[1.3, 0.9, 0.8, 1.8]
+prob_new = ODEProblem(lotka!, u0, (0.0f0, 40.0f0), p_)
+@time solution_new = solve(prob_new, AutoVern7(KenCarp4()), abstol = 1e-8, reltol = 1e-8, saveat = 0.25f0)
+y_zoh = ConstantInterpolation(solution_new[1,:], 0.0:0.25:40.0)
+prob_nn_updated = remake(prob_nn, p = res_final.u, u0 = u0, tspan = (0.0f0, 40.0f0))
+prediction_new = Array(solve(prob_nn_updated, AutoVern7(KenCarp4(autodiff = true)),  abstol = 1f-6, reltol = 1f-6,
+saveat = 0.25f0, sensealg = InterpolatingAdjoint(autojacvec = ReverseDiffVJP(true))))
+updated_obsgrid = 0.0:0.25:40.0
+t1 = t
+t3 = updated_obsgrid[41:123] |> collect
+gr()
+function plot_results(t, real, pred, pred_new)
+    plot(t1, pred[1,:], label = "Training Prediction", title="Training and Test Predictions of PEM Model", xlabel = "Time", ylabel = "Population")
+    plot!(t3, pred_new[1,41:123], label = "Test Prediction")
+    scatter!(t1, real[1,:], label = "Training Data")
+    scatter!(t3, solution_new[1,41:123], label = "Test Data")
+    vline!([t1[41]], label = "Training/Test Split")
+    plot!(legend=:topright)
+    savefig("Results/PEM LV Training and Testing.png")
+end
+
+plot_results(t, Xₙ, full_traj2, prediction_new)
