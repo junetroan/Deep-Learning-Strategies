86c1dbd547c62c9064b35161dc147d26c1d245cd
junetroan <junetroan@gmail.com>
2024-03-18 13:57:36 +0100

    Test of sim-HL-PEM file

--- Prediction Error Methods (PEM)/Simulations/Simulation Models/sim-HL-PEM.jl
+++ Prediction Error Methods (PEM)/Simulations/Simulation Models/sim-HL-PEM.jl
@@ -14,7 +14,7 @@ using Statistics
 using StatsBase
 gr()
 
-data_path = "lynx_hare_data.csv"
+data_path = "Multiple Shooting (MS)/ANODE-MS/Data/lynx_hare_data.csv"
 data = CSV.read(data_path, DataFrame)
 
 #Train/test Splits
@@ -29,6 +29,7 @@ lynx_data = data[:, 3]
 transformer = fit(ZScoreTransform, hare_data)
 X_train = StatsBase.transform(transformer, train[:, 2])
 X_test = StatsBase.transform(transformer, test[:, 2])
+y = X_train
 
 t = collect(1:size(data, 1))
 t_train = collect(1:Int(round(split_ration*size(data, 1))))
@@ -38,12 +39,11 @@ tspan = (t_train[1], t_train[end])
 tsteps = range(tspan[1], tspan[2], length = length(X_train))
 
 # Interpolation of given data
-y_zoh = ConstantInterpolation(X_train, tsteps)
+y_zoh = ConstantInterpolation(y, tsteps)
 
 # Definition of neural network
 iters = 2
 state = 2
-u0 = [X_train[1],0]
 
 @time begin
     for i in 1:iters
@@ -52,12 +52,12 @@ u0 = [X_train[1],0]
 
         rng1 = StableRNG(i+1)
         rng2 = StableRNG(i+2)
+        u0 = [y[1], randn(rng1, Float32, state-1)...]
+        K = rand(rng2, Float32, 2)
 
         U = Lux.Chain(Lux.Dense(state, 30, tanh),Lux.Dense(30, state))
         p, st = Lux.setup(rng1, U)
-
-        K = rand(rng2, Float32, 2)
-
+        
         function predictor!(du,u,p,t)
             û = U(u, p.vector_field_model, st)[1]
             yt = y_zoh(t)
@@ -66,8 +66,10 @@ u0 = [X_train[1],0]
         end
     
         params = ComponentVector{Float32}(vector_field_model = p, K = K)
-        prob_nn = ODEProblem(predictor!, u0 , tspan, params, saveat = 1.0f0 )
-        soln_nn = Array(solve(prob_nn, AutoTsit5(Rosenbrock23(autodiff=false)), abstol = 1e-8, reltol = 1e-8, saveat = 1.0f0 ))
+        prob_nn = ODEProblem(predictor!, u0 , tspan, params, saveat = tsteps )
+        soln_nn = solve(prob_nn, AutoTsit5(Rosenbrock23(autodiff=false)), abstol = 1e-8, reltol = 1e-8, saveat = 1.0f0 )
+        t = soln_nn.t
+        soln_nn = Array(soln_nn)
 
         function prediction(p)
             _prob = remake(prob_nn, u0 = u0, p = p)
@@ -75,15 +77,11 @@ u0 = [X_train[1],0]
             Array(solve(_prob, AutoTsit5(Rosenbrock23(autodiff=false)), abstol = 1e-8, reltol = 1e-8, saveat = tsteps , sensealg = sensealg))
         end
         
-        prediction(params)
-        
         function predloss(p)
             yh = prediction(p)
-            e2 = mean(abs2, X_train .- yh[1,:])
+            e2 = mean(abs2, y .- yh[1,:])
             return e2
         end
-        
-        predloss(params)
         
         losses = Float32[]
         callback = function (p, l)
@@ -98,23 +96,22 @@ u0 = [X_train[1],0]
         adtype = Optimization.AutoZygote()
         optf = Optimization.OptimizationFunction((x,p) -> predloss(x), adtype)
         optprob = Optimization.OptimizationProblem(optf, params)
-        res_ms = Optimization.solve(optprob, ADAM(), maxiters = 10000, verbose = false, callback=callback)
+        res_ms = Optimization.solve(optprob, ADAM(), maxiters = 5000, verbose = false, callback=callback)
         
         losses_df = DataFrame(losses = losses)
-        CSV.write("sim-FS-PEM/Loss Data/Losses $i.csv", losses_df, writeheader = false)
+        CSV.write("Prediction Error Methods (PEM)/Simulations/Results/sim-HL/Loss Data/Losses $i.csv", losses_df, writeheader = false)
         
         full_traj = prediction(res_ms.u)
         full_traj_loss = predloss(res_ms.u)
-        push!(fulltraj_losses, full_traj_loss)
         
         function plot_results(t, real, pred)
             plot(t, pred[1,:], label = "Training Prediction", title="Iteration $i of Randomised PEM Model", xlabel = "Time", ylabel = "Population")
             scatter!(t, real, label = "Training Data")
             plot!(legend=:topright)
-            savefig("sim-FS-PEM/Plots/Simulation $i.png")
+            savefig("Prediction Error Methods (PEM)/Simulations/Results/sim-HL/Plots/Simulation $i.png")
         end
         
-        plot_results(t, X_train, full_traj)
+        plot_results(t, y, full_traj)
 
     end
 end
