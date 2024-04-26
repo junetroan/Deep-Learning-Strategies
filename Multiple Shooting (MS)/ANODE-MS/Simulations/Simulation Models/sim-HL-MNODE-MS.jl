eb90553dd2fa3bd0d616060fc020b61c15945a92
junetroan <junetroan@gmail.com>
2024-04-26 10:51:44 +0200

    Finished sim-HL-MNODE-MS file

--- Multiple Shooting (MS)/ANODE-MS/Simulations/Simulation Models/sim-HL-MNODE-MS.jl
+++ Multiple Shooting (MS)/ANODE-MS/Simulations/Simulation Models/sim-HL-MNODE-MS.jl
@@ -72,10 +72,165 @@ tsteps = range(tspan[1], tspan[2], length = length(X_train))
 
 datasize
 groupsize
+group_size = groupsize
 X_train
 prob_node
+x = X_train
 
-##############################################################################################################################################################
+####################################################################################################################################################################
+
+# Simulation 
+
+####################################################################################################################################################################
+
+
+iters = 2
+
+@time begin
+    for i in 1:iters
+
+        println("Simulation $i")
+
+        # Random numbers
+        rng_1 = StableRNG(i)
+
+        # NEURAL NETWORK
+
+        U = Lux.Chain(Lux.Dense(state, 30, tanh),
+        Lux.Dense(30, state))
+
+        p, st = Lux.setup(rng_1, U)
+        params = ComponentVector{Float32}(vector_field_model = p)
+        neuralode = NeuralODE(U, tspan, AutoTsit5(Rosenbrock23(autodiff = false)), saveat = tsteps, sensealg = InterpolatingAdjoint(autojacvec = ReverseDiffVJP(true)))
+        prob_node = ODEProblem((u,p,t) -> U(u, p, st)[1][1:end], u0, tspan, ComponentArray(p))
+
+        function group_ranges(datasize::Integer, groupsize::Integer)
+            2 <= groupsize <= datasize || throw(DomainError(groupsize,
+            "datasize must be positive and groupsize must to be within [2, datasize]"))
+            return [i:min(datasize, i + groupsize - 1) for i in 1:(groupsize - 1):(datasize - 1)]
+        end
+
+        ranges = group_ranges(datasize, group_size)
+
+        u0 = Float32(X_train[first(1:5)])
+        u0_init = [[X_train[first(rg)]; fill(mean(X_train[rg]), state - 1)] for rg in ranges] 
+        u0_init = mapreduce(permutedims, vcat, u0_init)
+
+        function multiple_shoot_mod(p, ode_data, tsteps, prob::ODEProblem, loss_function::F,
+            continuity_loss::C, solver::SciMLBase.AbstractODEAlgorithm, group_size::Integer;
+            continuity_term::Real = 100, kwargs...) where {F, C}
+
+            datasize = size(ode_data, 1)
+
+            if group_size < 2 || group_size > datasize
+                throw(DomainError(group_size, "group_size can't be < 2 or > number of data points"))
+            end
+
+            ranges = group_ranges(datasize, group_size)
+
+            sols = [solve(remake(prob_node; p = p.θ, tspan = (tsteps[first(rg)], tsteps[last(rg)]),
+                u0 = p.u0_init[index, :]),
+                solver, saveat = tsteps[rg], sensealg = QuadratureAdjoint(autojacvec = ReverseDiffVJP(true))) 
+                for (index, rg) in enumerate(ranges)]
+
+            group_predictions = Array.(sols)
+
+            loss = 0
+
+            for (i, rg) in enumerate(ranges)
+                u = x[rg] # TODO: make it generic for observed states > 1
+                û = group_predictions[i][1, :]
+                loss += loss_function(u, û)
+
+                if i > 1
+                    # Ensure continuity between last state in previous prediction
+                    # and current initial condition in ode_data
+                    loss += continuity_term *
+                            continuity_loss(group_predictions[i - 1][:, end], group_predictions[i][:, 1])
+                end
+            end
+
+            return loss, group_predictions
+
+        
+        end
+
+        #Testing
+        # Calculate multiple shooting loss
+        loss_function(data, pred) = sum(abs2, data - pred)
+        continuity_loss(uᵢ₊₁, uᵢ) = sum(abs2, uᵢ₊₁ - uᵢ)
+        predict_single_shooting(p) = Array(first(neuralode(u0_init[1,:],p,st)))
+        tester_pred_ss = predict_single_shooting(params.vector_field_model)
+
+        function loss_single_shooting(p)
+            pred = predict_single_shooting(p)
+            l = loss_function(x, pred[1,:])
+            return l, pred
+        end
+
+        ls, ps = loss_single_shooting(params.vector_field_model)
+
+        continuity_term = 10.0
+        params = ComponentVector{Float32}(θ = p, u0_init = u0_init)
+        ls, ps = multiple_shoot_mod(params, x, tsteps, prob_node, loss_function, continuity_loss, AutoTsit5(Rosenbrock23(autodiff = false)), group_size; continuity_term)
+
+        # Modified multiple_shoot method 
+        multiple_shoot_mod(params, x, tsteps, prob_node, loss_function,
+            continuity_loss, AutoTsit5(Rosenbrock23(autodiff = false)), group_size;
+            continuity_term)
+
+        function loss_multiple_shoot(p)
+            return multiple_shoot_mod(p, x, tsteps, prob_node, loss_function,
+                continuity_loss, AutoTsit5(Rosenbrock23(autodiff = false)), group_size;
+                continuity_term)[1]
+        end
+   
+        losses = Float32[]
+
+        callback = function (θ, l)
+            push!(losses, loss_multiple_shoot(θ))
+            if length(losses) % 50 == 0
+                println("Current loss after $(length(losses)) iterations: $(losses[end])")
+
+            end
+            return false
+        end
+
+        adtype = Optimization.AutoZygote()
+        optf = Optimization.OptimizationFunction((x,p) -> loss_multiple_shoot(x), adtype)
+        optprob = Optimization.OptimizationProblem(optf, params)
+        @time res_ms = Optimization.solve(optprob, ADAM(),  maxiters = 5000, callback = callback) #callback  = callback,
+
+        losses_df = DataFrame(loss = losses)
+        CSV.write("Loss Data/HL Losses $i.csv", losses_df, writeheader = false)
+
+        loss_ms, _ = loss_single_shooting(res_ms.u.θ)
+        preds = predict_single_shooting(res_ms.u.θ)
+
+        function plot_results(real, pred)
+            plot(t_train, pred[1,:], label = "Training Prediction", title="Iteration $i of Randomised MNODE-MS Model", xlabel="Time", ylabel="Population")
+            scatter!(t_train, real, label = "Training Data")
+            plot!(legend=:topright)
+            savefig("Plots/HL Simulation $i.png")
+        end
+
+        plot_results(x, preds)
+
+        if i == iters
+            println("Simulation finished")
+            break 
+        end
+
+    end
+end
+
+
+
+##################################################################################################################################################################
+
+# Single round
+
+##################################################################################################################################################################
 
 function group_ranges(datasize::Integer, groupsize::Integer)
     2 <= groupsize <= datasize || throw(DomainError(groupsize,
@@ -227,3 +382,6 @@ function plot_results(real_train, real_test, train_pred, test_pred)
 end
 
 plot_results(X_train, X_test, preds, prediction_new)
+
+
+
